877a4cf8d2c59ad168b43c671a794382886c7b23
Sanjeev A <sanjeevganesh25@gmail.com>
2024-01-10 20:29:49 +0530

    Route and Header Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/get-started', pathMatch: 'full' },
+    { path: 'pricing', component: PricingComponent },
+    { path: 'about-us', component: AboutComponent },
+    { path: '**', redirectTo: '/get-started' } 
+];
