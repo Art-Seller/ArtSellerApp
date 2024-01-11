1a7a02772fdf23ce7ab75c24740bdd8f60a453c9
Sanjeev A <sanjeevganesh25@gmail.com>
2024-01-11 10:15:27 +0530

    Mobile Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { AboutComponent } from './pages/about/about.component';
 import { PricingComponent } from './pages/pricing/pricing.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/get-started', pathMatch: 'full' },
     { path: 'pricing', component: PricingComponent },
     { path: 'about-us', component: AboutComponent },
-    { path: '**', redirectTo: '/get-started' } 
 ];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage,NgClass],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  applogo="https://www.fnordware.com/superpng/pnggrad16rgb.png  "
+  
+  menuState:boolean = false;
+  toggleMenu(){
+    this.menuState = !this.menuState;
+  }
+
+  applogo="https://www.fnordware.com/superpng/pnggrad16rgb.png"
 }
